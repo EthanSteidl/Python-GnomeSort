1bebcf45f0614cb20717182c75d8de89390b749b
Ethan Steidl <60246470+EthanSteidl@users.noreply.github.com>
2020-12-08 17:07:34 -0700

    Add files via upload

---
+++ gnomesort.py
@@ -0,0 +1,8 @@
+def gnomeSort(a):
+    i = 0
+    while i < len(a):
+        if(i==0 or a[i] > a[i-1]):
+            i += 1
+        else:
+            a[i], a[i-1] = a[i-1], a[i]
+            i -= 1
